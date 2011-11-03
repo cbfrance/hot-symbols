f1c46562e86e5f8c01f540844cd650105a52a095
Chris Blow <unthinkingly@gmail.com>
2011-11-03 16:03:52 -0700

    cleanup routes

--- application.rb
+++ application.rb
@@ -2,7 +2,7 @@ require "sinatra"
 
 input_file="symbols.markdown"
 generated_file="views/generated.erb"
-header_pattern=/###/
+@header_pattern=/###/
 
 def to_classnames(a_string)
   a_string.strip.gsub("=", " ").gsub(":", " ")
@@ -16,19 +16,19 @@ def to_title(a_string)
   
 end
 
-def process_html(l)
+def process_html(file, l)
   unless l.empty? or l =="\n"
-    if (l =~ header_pattern)
-      file << "<h2>"  + l.gsub(header_pattern, "").strip + "</h2>" + "\n"
+    if (l =~ @header_pattern)
+      file << "<h2>"  + l.gsub(@header_pattern, "").strip + "</h2>" + "\n"
     else
       file << "<div class='item " + to_classnames(l) + "'>"  + "<object data='icons/#{to_file_name(l)}.svg' type='image/svg+xml' width='50' height='50'></object>" + "<object class='large' data='icons/#{to_file_name(l)}.svg' type='image/svg+xml' width='50' height='50'></object>" + l.strip + "</a></div>" + "\n <span class='attribution'>attribution</span>"
     end
   end
 end
 
-def process_mediawiki(l)
+def process_mediawiki(file, l)
   unless l.empty? or l =="\n"
-    if (l =~ header_pattern)
+    if (l =~ @header_pattern)
       # skip
     else
       file << to_classnames(l) + "[['icons/#{to_file_name(l)}.svg' width='50' height='50']]" + "\n <span class='attribution'>attribution</span>"
@@ -36,19 +36,17 @@ def process_mediawiki(l)
   end
 end
 
-
-
-get "/generate" do # recreate and read the generated file
+get "/generate/:format" do # recreate and read the generated file
   
   File.delete(generated_file)
   
   open(generated_file, "a+") do |file|
     input = File.open(input_file, "r").readlines
     input.each do |l|
-      if params[:process_mediawiki]
-        process_mediawiki(l)
+      if params[:format] == "mediawiki"
+        process_mediawiki(file, l)
       else
-        process_html(l)
+        process_html(file, l)
       end
     end
   end
